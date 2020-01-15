957da11b7ce0a72da1a1553f9d093bf0fa6498d6
Heather Nye <heatherdnye@gmail.com>
2020-01-14 19:12:16 -0600

    snapshot test added

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,17 @@
 // Test away!
+import React from 'react';
+import * as rtl from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import Display from './Display';
+
+test('Display is unlocked', () => {
+    const wrapper = rtl.render(<Display />);
+    const element = wrapper.getByText(/unlocked/i);
+    expect(element).toBeVisible();
+})
+
+
+test('<Display /> snapshot', () => {
+    const wrapper = rtl.render(<Display />)
+    expect(wrapper.asFragment()).toMatchSnapshot();
+})
